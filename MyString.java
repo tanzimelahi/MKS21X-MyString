3e4ef8f4161ea77f45ece5cbd308ae79edcec587
tanzimelahi <telahi00@stuy.edu>
2018-11-25 22:42:25 -0500

    testd a couple, fixed charsequence and string issue in subsequence

--- MyString.java
+++ MyString.java
@@ -1,27 +1,38 @@
-public class MyString extends CharSequence,Comparable<CharSequence>{
-   private char[]data;
-   public MyString(CharSequece s){
-     data=char[s.length];
-     for (int i=0;i<s.length();s++){
-       data[i]=s.charAt(i);
-     }
-   }
-   public char charAt(int index){
-     return data[index];
-   }
-   public  CharSequece subsequence(int start,int end){
-     String result="";
-     for(int i=start;i<end;i++){
-       result+=data[i];
-     }
-     return result;
-   }
-   public String toString(){
-     String result="";
-     for(int i=0;i<data.length;i++){
-       result+=data[i];
-     }
-     return result;
-   }
-   public String  comparable
+public class MyString implements CharSequence,Comparable<CharSequence>{
+	   private char[]data;
+	   public MyString(CharSequence s){
+	     data=new char[s.length()];
+	     for (int i=0;i<s.length();s++){
+	       data[i]=s.charAt(i);
+	     }
+	   }
+	   public char charAt(int index){
+	     return data[index];
+	   }
+	   public  CharSequence subSequence(int start,int end){
+	     CharSequence result="";
+	     for(int i=start;i<end;i++){
+	       result+=result+data[i];
+	     }
+	     return result;
+	   }
+	   public String toString(){
+	     String result="";
+	     for(int i=0;i<data.length;i++){
+	       result+=data[i];
+	     }
+	     return result;
+	   }
+	   public int length() {
+		   int size=0;
+		   for(int i=0;i<data.length;i++) {
+			   size++;
+		   }
+		   return size;
+	   }
+	  
+		   
+	
 }
+
+
