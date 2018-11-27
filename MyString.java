8ff5ad0cfd4089de891ada98921f08a3f9c37932
tanzimelahi <telahi00@stuy.edu>
2018-11-26 20:26:26 -0500

    finally compiles,checked compilation errrors all methods completed,exceptions yet not added

--- MyString.java
+++ MyString.java
@@ -1,36 +1,49 @@
-public class MyString implements CharSequence{
-	     data=new char[s.length()];
-	     for (int i=0;i<s.length();i++){
-	       data[i]=s.charAt(i);
-	     }
-	   }
-	   public char charAt(int index){
-	     return data[index];
-	   }
-	   public  CharSequence subSequence(int start,int end){
-	     String  result="";
-	     for(int i=start;i<end;i++){
-	       result+=result+data[i];
-	     }
-	     return result;
-	   }
-	   public String toString(){
-	     String result="";
-	     for(int i=0;i<data.length;i++){
-	       result+=data[i];
-	     }
-	     return result;
-	   }
-	   public int length() {
-		   int size=0;
-		   for(int i=0;i<data.length;i++) {
-			   size++;
-		   }
-		   return size;
-	   }
-	  
-		   
-	
+public class MyString implements  CharSequence,Comparable<CharSequence>{
+      private char[]data;
+      public MyString(CharSequence s){
+         data=new char[s.length()];
+        for(int i=0;i<s.length();i++){
+             data[i]=s.charAt(i);
+            }
+       }
+       public int length(){
+         return data.length;
+       }
+       public String toString(){
+         String result="";
+         for(int i=0;i<data.length;i++){
+           result+=data[i];
+         }
+         return result;
+       }
+       public CharSequence subSequence(int start,int end){
+         String result="";
+         for(int i=start;i<end;i++){
+           result+=data[i];
+         }
+         return result;
+       }
+       public char charAt(int index){
+         return data[index];
+       }
+       public int compareTo(CharSequence s){
+         int minLength;
+         if(s.length()<=this.length()){
+           minLength=s.length();
+         }
+         else{
+           minLength=this.length();
+         }
+        for(int i=0;i<minLength;i++){
+            if(! (this.charAt(i)-s.charAt(i)==0)){
+            return  this.charAt(i)-s.charAt(i);
+             }
+        }
+        if(s.length()==this.length()){
+          return 0;
+        }
+        else{
+          return this.length()-s.length();
+        }
+     }
 }
-
-
